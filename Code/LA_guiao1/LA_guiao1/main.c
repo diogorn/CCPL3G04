3748a7b863f70f8898ca9d908d4f1144ab90fd64
Diogo Neto <77453924+diogorn@users.noreply.github.com>
2021-03-20 17:19:13 +0000

    guiao1

---
+++ Code/LA_guiao1/LA_guiao1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LA_guiao1
+//
+//  Created by Diogo Neto on 20/03/2021.
+//
+
+#include <stdio.h>
+
+int main() {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
