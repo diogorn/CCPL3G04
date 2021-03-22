12583e1a58795599b1d57d27f652553e1b09236b
Sarafont <79592636+Sarafont@users.noreply.github.com>
2021-03-22 21:47:24 +0000

    Update parser.c

--- Code/LA_guiao1/LA_guiao1/parser.c
+++ Code/LA_guiao1/LA_guiao1/parser.c
@@ -1,4 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "parser.h"
 
 // analisa as variaveis e a operação
 
+void myparser (char *line){
+    char *delims = " \t\n";
+    
+    for (char *token = strtok(line, delims); token != NULL; token = strtok(NULL, delims)){
+        printf("%s\n", token);
+    }
+}
