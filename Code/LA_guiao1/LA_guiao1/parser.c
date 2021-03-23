2b958b701e0fe9dc5aac1c9ce1b242be92839651
Sarafont <79592636+Sarafont@users.noreply.github.com>
2021-03-23 12:52:47 +0000

    Update parser.c

--- Code/LA_guiao1/LA_guiao1/parser.c
+++ Code/LA_guiao1/LA_guiao1/parser.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "parser.h"
 
-void myparser (char *line){
+void parser(char *linha){
     char *delims = " \t\n";
     
-    for (char *token = strtok(line, delims); token != NULL; token = strtok(NULL, delims)){
+    for (char *token = strtok(line, delims); token != NULL; token = strtok(NULL,delims)){
         char *sobra;
         long val_i = strtol(token, &sobra, 10);
-        if (strlen (sobra) == 0){
-            printf("INT: %ld\n", val_i);
-        } else {
-             double val_d = strtod(token, &sobra);
-             if (strlen (sobra)==0){
-                   printf("DOUBLE: %lf\n", val_d);}
-             } else {
-                 printf("%s\n", token); }
+        if(strlen(sobra) == 0){
+                 PUSH(val_i);
+        } else if (strcmp(token, "+") == 0){
+                  long y = POP();
+                  long x = POP();
+                  PUSH(x + y);
+        } else if (strcmp(token, "-") == 0){
+                  long y = POP();
+                  long x = POP();
+                  PUSH(x - y);
+        } else if (strcmp(token, "*") == 0){
+                  long y = POP();
+                  long x = POP();
+                  PUSH(x * y);
+        } else if (strcmp(token, "/") == 0){
+                  long y = POP();
+                  long x = POP();
+                  PUSH(x / y);
+        } else if (strcmp(token, "#") == 0){
+                  long y = POP();
+                  long x = POP();
+                  PUSH(x # y);
+        } else if (strcmp(token, "") == 0){
+                  long y = POP();
+                  long x = POP();
+                  PUSH(x - y);
+        }
+        
+    }
 }
-
