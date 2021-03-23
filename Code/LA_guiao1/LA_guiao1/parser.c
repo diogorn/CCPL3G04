6f499385e55813c444163d73d81ad2ef7b6bbad0
pmrobraga <79986282+pmrobraga@users.noreply.github.com>
2021-03-23 15:42:07 +0000

    Update parser.c

--- Code/LA_guiao1/LA_guiao1/parser.c
+++ Code/LA_guiao1/LA_guiao1/parser.c
@@ -40,5 +40,11 @@ void parser(char *linha){
                   long y = POP();
                   long x = POP();
                   PUSH(x | y);
-    }
-}
+        } else if (strcmp(token, "~") == 0){
+                  long y = POP();
+                  long x = POP();
+                  PUSH(x ~ y);
+      }
+        
+ }        
+      
