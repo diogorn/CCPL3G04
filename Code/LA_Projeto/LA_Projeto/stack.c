a7d044e14e4dd2d90d4c72a16c76b153b247ec88
Sarafont <79592636+Sarafont@users.noreply.github.com>
2021-03-25 17:44:13 +0000

    Update stack.c

--- Code/LA_Projeto/LA_Projeto/stack.c
+++ Code/LA_Projeto/LA_Projeto/stack.c
@@ -9,6 +9,14 @@ MyStack inicia_Stack (){
     return p;
 }
 
+void PRINT_STACK(MyStack *p){
+    int i;
+    for (i=0; i<p->topo; i++){
+        printf("%ld",p->a[i]);
+    }
+    printf("\n");
+}
+
 int vazia (MyStack p){
     if(p.topo == -1){
         return 1;
@@ -25,25 +33,22 @@ int cheia (MyStack p){
     }
 }
 
-void PUSH (MyStack p, long x){
-    if(cheia(MyStack p) == 1){
+void PUSH (MyStack *p, long x){
+    if(cheia(*p) == 1){
         printf ("Pilha Cheia!");
     } else {
-        p.topo++;
-        p.a[p.topo] = x; //vamos guardar a variavel x e guardar dentro da pilha na posição topo
+        p->topo++;
+        p->a[p->topo] = x; //vamos guardar a variavel x e guardar dentro da pilha na posição topo
     }
 }
 
-int POP (MyStack p){
+int POP (MyStack *p){
     int aux; // recebe o valor da pilha para depois retornar
-    if(vazia (MyStack p) == 1){
+    if(vazia (*p) == 1){
         printf ("Pilha Vazia!");
-        return 
     } else {
-        aux = p.a[p.topo];
-        p.topo--;
+        aux = p->a[p->topo];
+        p->topo--;
         return aux;
     }
 }
-
- // porque é que está a pedir uma expressão antes no MyStack na linha 28 e 38
