dbbacf711e8b6135ff3487ce1e22f483e15bfdbe
Sarafont <79592636+Sarafont@users.noreply.github.com>
2021-03-25 17:47:07 +0000

    Update parser.c

--- Code/LA_Projeto/LA_Projeto/parser.c
+++ Code/LA_Projeto/LA_Projeto/parser.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "stack.h"
 #include "parser.h"
@@ -8,61 +9,62 @@
 void parser(char *linha){
     char *delims = " \t\n";
 
-    MyStack p = inicia_Stack ();
-    
+    MyStack s = inicia_Stack ();
+    MyStack *p = &s;
+
     for (char *token = strtok(linha, delims); token != NULL; token = strtok(NULL,delims)){
         char *sobra;
         long val_i = strtol(token, &sobra, 10);
         if(strlen(sobra) == 0){
-                 PUSH(MyStack p,val_i);
+                 PUSH(p,val_i);
         } else if (strcmp(token, "+") == 0){
-                  long y = POP(MyStack p);
-                  long x = POP(MyStack p);
-                  PUSH(MyStack p, (x + y));
+                  long y = POP(p);
+                  long x = POP(p);
+                  PUSH(p, (x + y));
         } else if (strcmp(token, "-") == 0){
-                  long y = POP(MyStack p);
-                  long x = POP(MyStack p);
-                  PUSH(MyStack p, (x - y));
+                  long y = POP(p);
+                  long x = POP(p);
+                  PUSH(p, (x - y));
         } else if (strcmp(token, "*") == 0){
-                  long y = POP(MyStack p);
-                  long x = POP(MyStack p);
-                  PUSH(MyStack p, (x * y));
+                  long y = POP(p);
+                  long x = POP(p);
+                  PUSH(p, (x * y));
         } else if (strcmp(token, "/") == 0){
-                  long y = POP(MyStack p);
-                  long x = POP(MyStack p);
-                  PUSH(MyStack p, (x / y));
+                  long y = POP(p);
+                  long x = POP(p);
+                  PUSH(p, (x / y));
         } else if (strcmp(token, "#") == 0){
-                  long y = POP(MyStack p);
-                  long x = POP(MyStack p);
-                  PUSH(MyStack p, (x # y));
+                  long y = POP(p);
+                  long x = POP(p);
+                  PUSH(p, pow(x,y));
         } else if (strcmp(token, "&") == 0){
-                   long y = POP(MyStack p);
-                   long x = POP(MyStack p);
-                   PUSH(MyStack p, (x & y));
+                   long y = POP(p);
+                   long x = POP(p);
+                   PUSH(p, (x & y));
         } else if (strcmp(token, "|") == 0){
-                   long y = POP(MyStack p);
-                   long x = POP(MyStack p);
-                   PUSH(MyStack p, (x | y));
+                   long y = POP(p);
+                   long x = POP(p);
+                   PUSH(p, (x | y));
         } else if (strcmp(token, "^") == 0){
-                   long y = POP(MyStack p);
-                   long x = POP(MyStack p);
-                   PUSH(MyStack p, (x ^ y));
+                   long y = POP(p);
+                   long x = POP(p);
+                   PUSH(p, (x ^ y));
         } else if (strcmp(token, "~") == 0){
-                   long y = POP(MyStack p);
-                   long x = POP(MyStack p);
-                   PUSH(MyStack p, (x ~ y));
-        } } else if (strcmp(token, "%")==0){
-                  long y = POP(MyStack p);
-                  long x = POP(MyStack p);
-                  PUSH(MyStack p,(x % y));
+                   long x = POP(p);
+                   PUSH(p, ~ x);
+        } else if (strcmp(token, "%")==0){
+                  long x = POP(p);
+                  PUSH(p,(abs(x)));
         } else if (strcmp(token, "(")==0){
-                  long x = POP(MyStack p);
-                  PUSH(MyStack p, (x+1));
+                  long x = POP(p);
+                  PUSH(p, (x+1));
         } else if (strcmp(token, ")")==0){
-                  long x = POP(MyStack p);
-                  PUSH(MyStack p, (x-1));
+                  long x = POP(p);
+                  PUSH(p, (x-1));
         }
-    PRINT_STACK();
+    PRINT_STACK(*p);
+    }
 }
+
 
 
