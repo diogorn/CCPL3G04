c93bd732d0d94400f356b38cabfa5098e99d11ff
Barbarabrito19 <75738104+Barbarabrito19@users.noreply.github.com>
2021-04-08 12:38:50 +0100

    Update parser.c

--- GUIÃO2/parser.c
+++ GUIÃO2/parser.c
@@ -19,69 +19,97 @@ void parser(char *linha){
     for (char *token = strtok(linha, delims); token != NULL; token = strtok(NULL,delims)){
         char *sobra;
         long val_i = strtol(token, &sobra, 10);
+        
         if(strlen(sobra) == 0){
                  PUSH(p,val_i);
+        
         } else if (strcmp(token, "+") == 0){
                   long y = POP(p);
                   long x = POP(p);
                   PUSH(p, (x + y));
+        
         } else if (strcmp(token, "-") == 0){
                   long y = POP(p);
                   long x = POP(p);
                   PUSH(p, (x - y));
+        
         } else if (strcmp(token, "*") == 0){
                   long y = POP(p);
                   long x = POP(p);
                   PUSH(p, (x * y));
+        
         } else if (strcmp(token, "/") == 0){
                   long y = POP(p);
                   long x = POP(p);
                   PUSH(p, (x / y));
+        
         } else if (strcmp(token, "#") == 0){
                   long y = POP(p);
                   long x = POP(p);
                   PUSH(p, pow(x,y));
+        
         } else if (strcmp(token, "&") == 0){
                    long y = POP(p);
                    long x = POP(p);
                    PUSH(p, (x & y));
+        
         } else if (strcmp(token, "|") == 0){
                    long y = POP(p);
                    long x = POP(p);
                    PUSH(p, (x | y));
+        
         } else if (strcmp(token, "^") == 0){
                    long y = POP(p);
                    long x = POP(p);
                    PUSH(p, (x ^ y));
+        
         } else if (strcmp(token, "~") == 0){
                    long x = POP(p);
                    PUSH(p, ~ x);
-        } else if (strcmp(token, "%")==0){
+        
+        } else if (strcmp(token, "%") == 0){
                   long x = POP(p);
                   long y = POP(p);
                   PUSH(p,(y%x));
-        } else if (strcmp(token, "(")==0){
+        
+        } else if (strcmp(token, "(") == 0){
                   long x = POP(p);
                   PUSH(p, (x-1));
-        } else if (strcmp(token, ")")==0){
+        
+        } else if (strcmp(token, ")") == 0){
                   long x = POP(p);
                   PUSH(p, (x+1));
-        } else if (strcmp(token, "_")==0){
+        
+        } else if (strcmp(token, "_") == 0){
                   long x = POP(p);
                   PUSH(p, x);
                   PUSH(p, x);
-        }// else if (strcmp(token, ";")){
-//                  long x = POP(p);
-//                  long y = POP(p);
-//                  PUSH(p, y);
-//
-//        } else if (strcmp(token, "\\")){
-//            long x = POP(p);
-//            long y = POP(p);
-//            PUSH(p, y);
-//            PUSH(p, x);
-//        }
-//
+        
+        } else if (strcmp(token, ";") == 0){
+                  long x = POP(p);
+                  long y = POP(p);
+                  PUSH(p, y);
+                 
+        } else if (strcmp(token, "\\") == 0){
+            long x = POP(p);
+            long y = POP(p);
+            PUSH(p, y);
+            PUSH(p, x);
+
+        } else if (strcmp(token, "@") == 0){
+            long x = POP(p);
+            long y = POP(p);
+            long z = POP(p);
+            PUSH(p, y);
+            PUSH(p, z);
+            PUSH(p, x);
+
+        } else if (strcmp(token, "$") == 0){
+            long x = POP(p);
+            long y = POP(p);
+            PUSH(p, x);
+            PUSH(p, x);
+        }                          
     }
     PRINT_STACK(p);
 }
