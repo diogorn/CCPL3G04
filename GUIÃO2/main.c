a3cab4d0b9e55a6522a7bf15464eb674dbfa2008
Sarafont <79592636+Sarafont@users.noreply.github.com>
2021-04-08 12:35:34 +0100

    Update main.c

--- GUIÃO2/main.c
+++ GUIÃO2/main.c
@@ -1,21 +1,27 @@
-#include <stdio.h>  // esta biblioteca define algumas funções e macros (como NULL) relativas às operações de imput/output
-#include <assert.h> // esta biblioteca define uma função (assert) utilizada para verificar suposições feitas pelo programa. Sempre que a expressão passada como argumento é falsa, escreve uma mensagem de diagnostico
-#include <stdlib.h> // esta biblioteca possui funções envolvendo alocação de memória, controle de processos e conversões
-#include <string.h> // esta biblioteca cria funções que permitem manipular strings e caracteres
+/**
+ * @file Ficheiro que contém a função principal de todo o programa
+ */
+#include <stdio.h>  
+#include <assert.h> 
+#include <stdlib.h> 
+#include <string.h> 
 
 #include "parser.h"
 #include "stack.h"
 
-#define SIZE 10240 // definimos uma macro para o tamanho da linha, neste caso 10204 caracteres
+#define SIZE 10240 
 
-
+/**
+ * Função função principal do programa.
+ * @return O valor 0
+ */
 int main() {
-    char linha[SIZE]; // definimos uma variavel do tipo char com o tamanho definido na macro
+    char linha[SIZE]; 
     
-    assert( fgets(linha, SIZE, stdin) != NULL); // Lê a sequência de caractres ate encontrar o caracter NULL
-    assert( linha[strlen(linha) - 1] == '\n'); //Lê uma sequência de caracteres até encontrar um \n. O limite é sempre um caractere a menos que o especificado porque no final é incluso um caractere NULL para terminar a string.
+    assert( fgets(linha, SIZE, stdin) != NULL); 
+    assert( linha[strlen(linha) - 1] == '\n'); 
     
-    parser(linha);  //coloca a linha obtida na função parser
+    parser(linha); 
     
     return 0;
 }
