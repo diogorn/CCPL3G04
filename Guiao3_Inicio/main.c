c7c20960926fdcf794db0b87d8627f946333640c
Diogo Neto <77453924+diogorn@users.noreply.github.com>
2021-05-01 14:22:50 +0100

    Update main.c

--- Guiao3_Inicio/main.c
+++ Guiao3_Inicio/main.c
@@ -8,23 +8,9 @@
 #define SIZE 10240
 
 int main (){
-    char line[SIZE];
-//    assert(fgets(line, SIZE, stdin) != NULL);
-//    assert(line[strlen(line -1)] == '\n'); // se o ultimo caracter for \n, a função calcula o comprimento da line e tira-lhe 1
-
-    
+    char line[SIZE];    
     parser(fgets(line, SIZE, stdin));
     
     return 0;
 }
-//65 - 10
-//66 - 11
-//67 - 12
-//68 - 13
-//69 - 14
-//70 - 15
-//78 - \n
-//83 - " "
-//88 - 0
-//89 - 1
-//90 - 2
+
