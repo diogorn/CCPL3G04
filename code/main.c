a2e6a386b754c69b3ee6d6357baf0f6709b2230a
Sarafont <79592636+Sarafont@users.noreply.github.com>
2021-05-06 22:53:08 +0100

    Update main.c

--- code/main.c
+++ code/main.c
@@ -1,3 +1,6 @@
+/**
+ * @file Este ficheiro contém a função parser
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -7,6 +10,11 @@
 
 #define SIZE 10240
 
+/**
+ * \brief A função parser faz o parser de uma dada linha atribuida como argumento
+ * 
+ * @param line É a linha que foi lida e da qual se vai fazer o parser
+ */
 int main (){
     char line[SIZE];
     
